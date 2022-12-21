1db4c13ed7561349b175eb602f6679eb16abffe8
ilker m. sitki <ilkermeliksitki@proton.me>
2022-12-21 15:29:39 +0300

    add test cases and test file

---
+++ week-1/tests/1_brackets_in_code/test_brackets_in_code.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+gcc -g -pipe -O0 -std=c11 ../../../week-1/brackets_in_code.c -lm
+for i in {01..54}
+do
+    result=$(cat cases/$i | ./a.out)
+    test=$(cat cases/$i.a)
+    #echo $result $test
+    if [ "$result" != "$test" ]
+    then
+        echo $i case failed.
+        echo $(cat cases/$i)
+    fi
+done
