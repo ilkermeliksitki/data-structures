7fef128a948b796cc1771595f147f31bb26b3482
ilker m. sitki <ilkermeliksitki@proton.me>
2022-12-23 17:01:16 +0300

    Add some improvements to stdout

--- week-1/tests/1_brackets_in_code/test_brackets_in_code.sh
+++ week-1/tests/1_brackets_in_code/test_brackets_in_code.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-gcc -g -pipe -O0 -std=c11 ../../../week-1/brackets_in_code.c -lm
+gcc -g -pipe -O0 -std=c11 ../../../week-1/brackets_in_code.c -lm &&
 for i in {01..54}
 do
     result=$(cat cases/$i | ./a.out)
     test=$(cat cases/$i.a)
-    #echo $result $test
     if [ "$result" != "$test" ]
     then
         echo the case $i failed.
         echo $(cat cases/$i)
+        echo your result =\> $result
+        echo test result =\> $test
+        printf "\n"
     fi
 done
