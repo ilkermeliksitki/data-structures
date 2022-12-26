176cdb1ca6e188a842272a013c5f54d15fc7c399
ilker m. sitki <ilkermeliksitki@proton.me>
2022-12-27 02:01:52 +0300

    change it to non-binary version, but inefficient(test case 16)

--- week-1/tree_height.c
+++ week-1/tree_height.c
@@ -3,76 +3,85 @@
 
 struct node {
     int key;
-    struct node *left;
-    struct node *right;
+    int num_children;
+    struct node** children;
 };
 
-struct node* create_node(int key, struct node* l, struct node* r)
+struct node* create_node(int key, int num_children)
 {
-    struct node* n = (struct node*)malloc(sizeof(struct node));
+    struct node* n = (struct node*)malloc(sizeof(struct node*));
     n->key = key;
-    n->left = l;
-    n->right = r;
+    n->num_children = num_children;
+    n->children = (struct node**)malloc(num_children * sizeof(struct node*));
     return n;
 }
 
-void fill_node(struct node* nodes[], int n, int val, int pos)
+void add_child(int parent_key, int child_key, struct node* nodes[], int n)
 {
     for (int i = 0; i < n; i++){
-        if (nodes[i]->key == val) {
-            if (nodes[i]->left == NULL) {
-                nodes[i]->left = nodes[pos];
-            } else if (nodes[i]->right == NULL){
-                nodes[i]->right = nodes[pos];
-            } else {
-                printf("%s\n", "wrong format");
-                return;
+        if (nodes[i]->key == parent_key) {
+            for (int j = 0; j < nodes[i]->num_children; j++){
+                if (nodes[i]->children[j] == NULL){
+                    nodes[i]->children[j] = nodes[child_key];
+                    return;
+                }
             }
-            break;
         }
     }
 }
 
-
 int tree_height(struct node* root)
 {
-    if (root == NULL) {
+    if (root == NULL)
         return 0;
+
+    int curr_height = 0;
+    for (int i = 0; i < root->num_children; i++){
+        int child_height = tree_height(root->children[i]);
+        if (child_height > curr_height){
+            curr_height = child_height;
+        }
     }
-    int left_height = tree_height(root->left);
-    int right_height = tree_height(root->right);
-    return 1 + (left_height < right_height ? right_height : left_height);
+    return 1 + curr_height;
 }
 
 int main(void)
 {
     int n;
     scanf("%d", &n);
+    int keys[n];
+    for (int i = 0; i < n; i++){
+        scanf("%d", &keys[i]);
+    }
 
+    int num_children_arr[n];
+    for (int i = 0; i < n; i++){
+        num_children_arr[i] = 0;
+    }
+
+    int root_idx;
+    for (int i = 0; i < n; i++){
+        if (keys[i] != -1) {
+            num_children_arr[keys[i]]++;
+        } else {
+            root_idx = i;
+        }
+    }
     struct node* nodes[n];
     for (int i = 0; i < n; i++){
-        nodes[i] = create_node(i, NULL, NULL);
+        nodes[i] = create_node(i, num_children_arr[i]);
+    }
+    for (int i = 0; i < n; i++){
+        add_child(keys[i], i, nodes, n);
     }
 
-    int val, root_idx;
-    for (int i = 0; i < n; i++){
-        scanf("%d", &val);
-        if (val == -1) {
-            root_idx = i;
-            continue;
-        }
-        fill_node(nodes, n, val, i);
-    }
-    
-    struct node* root_node;
-    root_node = nodes[root_idx];
-    int h = tree_height(root_node);
+    int h = tree_height(nodes[root_idx]);
     printf("%d\n", h);
 
-    /* deallocate the allocated part of the heap */
+    /* deallocation part */
     for (int i = 0; i < n; i++){
         free(nodes[i]);
     }
-
+    
     return 0; 
 }
