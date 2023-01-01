eaf7cf47b30b8c4781095d2b735c299fb7198082
ilker m. sitki <ilkermeliksitki@proton.me>
2023-01-01 16:21:04 +0300

    add test file of network simulation

---
+++ week-1/tests/3_network_simulation/test_network_simulation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+gcc -g -pipe -O0 -std=c11 ../../../week-1/network_simulation.c -lm &&
+for i in {01..22}
+do
+    result=$(cat cases/$i | ./a.out)
+    test=$(cat cases/$i.a)
+    if [ "$result" != "$test" ]; then
+        echo the case $i failed.
+        echo $(cat cases/$i)
+        echo your result =\> $result
+        echo test result =\> $test
+        printf "\n"
+        exit
+    else
+        echo passed $i
+    fi
+done
+echo All test passed.
