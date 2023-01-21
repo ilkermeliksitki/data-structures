aac8a390b67a3e652347200133fd1059fe9dcaf1
ilker m. sitki <ilkermeliksitki@proton.me>
2023-01-22 00:21:43 +0300

    add test function for merging tables

---
+++ week-3/tests/3_merging_tables/test_merging_tables.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+gcc -g -pipe -O0 -std=c11 ../../../week-3/merging_tables.c -lm &&
+for i in {00..04}
+do
+    result=$(cat cases/$i | ./a.out)
+    test=$(cat cases/$i.a)
+    if [ "$result" != "$test" ]; then
+        echo the case $i failed.
+        echo $(cat cases/$i)
+        echo your result =\> $result
+        echo test result =\> $test
+        printf "\n"
+        exit
+    else
+        echo passed $i
+    fi
+done
+echo "All test passed."
