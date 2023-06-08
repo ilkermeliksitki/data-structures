b5d68b773446da5a3686635a2be05b967be1cd05
ilker m. sitki <ilkermeliksitki@proton.me>
2023-06-08 23:21:25 +0300

    add valgrind for memory leak checking
    Changes to be committed:
    	modified:   test_tree_traversals.sh

--- week-6/tests/1_tree_traversals/test_tree_traversals.sh
+++ week-6/tests/1_tree_traversals/test_tree_traversals.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
 
-# Compile the C code
+# compile the C code with debugging symbols
 gcc -g -pipe -O0 -std=c11 ../../../week-6/tree_traversals.c -lm -o a.out
 
-# Define an array of test case numbers
+# define an array of test case numbers
 test_cases=("01" "02" "03")
 
-# Define red color
+
+# colors
 RED='\033[0;31m'
+RED_BOLD='\033[1;31m'
+GREEN_BOLD='\033[1;32m'
 NC='\033[0m'    # No Color
 
-# Iterate over the test case files
+# iterate over the test case files
 for i in "${test_cases[@]}"
 do
-    # Execute the program and capture the output
+    # execute the program using Valgrind and capture the output
+    valgrind_output=$(valgrind --leak-check=full ./a.out < "cases/$i" 2>&1)
+
+    # check if Valgrind reported any memory leaks
+    if [[ "$valgrind_output" =~ "All heap blocks were freed -- no leaks are possible" ]]; then
+        memory_leak_info="${GREEN_BOLD}without memory leak${NC}"
+    else
+        memory_leak_info="${RED_BOLD}with memory leak${NC}"
+    fi
+
+    # execute the program again and capture the program's output
     result=$(./a.out < "cases/$i")
 
-    # Read the expected output from the file
+    # read the expected output from the file
     expected=$(cat "cases/$i.a")
 
-    # Compare the result and the expected output
-    # If there is a trailing space, it will appears as a red block character.
+    # compare the result and the expected output
+    # If there is a trailing space, it will appear as a red block character.
     if [ "$result" != "$expected" ]; then
-        echo "Test case $i failed."
+        echo -e "Test case $i failed ${memory_leak_info}"
         echo "Input:"
         cat "cases/$i"
         echo -e "\nYour result:"
@@ -32,7 +45,7 @@ do
         echo
         exit 1
     else
-        echo "Test case $i passed."
+        echo -e "Test case $i passed ${memory_leak_info}"
     fi
 done
 
